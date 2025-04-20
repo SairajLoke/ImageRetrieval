90e3b48c9670200e5a1c56fd19294a60b12aa45f
Yashiiti <yashvashishtha03@gmail.com>
2025-04-20 11:59:58 +0530

    add code for feature extraction still need to merge it properly into current code structure

---
+++ CMakeFiles/image_retrieval.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/image_retrieval.dir/main.cpp.o"
+  "CMakeFiles/image_retrieval.dir/main.cpp.o.d"
+  "CMakeFiles/image_retrieval.dir/src/distance_metrics.cpp.o"
+  "CMakeFiles/image_retrieval.dir/src/distance_metrics.cpp.o.d"
+  "CMakeFiles/image_retrieval.dir/src/feature_extractor.cpp.o"
+  "CMakeFiles/image_retrieval.dir/src/feature_extractor.cpp.o.d"
+  "CMakeFiles/image_retrieval.dir/src/knn.cpp.o"
+  "CMakeFiles/image_retrieval.dir/src/knn.cpp.o.d"
+  "bin/image_retrieval"
+  "bin/image_retrieval.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/image_retrieval.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
